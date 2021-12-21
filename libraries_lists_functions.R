e75b7bebf0283a51b50b8c9bd09c1e9cca730507
B Steele <steeleb@caryinstitute.org>
2021-12-21 11:29:37 -0700

    2021 data cleaning complete

--- libraries_lists_functions.R
+++ libraries_lists_functions.R
@@ -56,5 +56,8 @@ therm <- c('temp_C_0.5m', 'temp_C_1m', 'temp_C_2m', 'temp_C_4m',
            'temp_C_22m', 'temp_C_30m')
 weather <- c('wind_dir_deg', 'wind_sp_mps', 'ave_wind_sp_mps', 
              'air_temp_degC', 'rel_hum_perc', 'bar_press_mmhg', 'daily_cum_rain_cm')
+weather_proc <- c('wind_dir_deg', 'wind_sp_mps', 'ave_wind_sp_mps', 
+                  'air_temp_degC', 'rel_hum_perc', 'bar_press_mmhg', 'rain_cm_hr')
+wind <- c('wind_dir_deg', 'wind_sp_mps', 'ave_wind_sp_mps')
 do1m = c('do_ppm_1m', 'dotemp_C_1m', 'do_sat_pct_1m')
 do14m = c('dotemp_C_14.5m', 'do_sat_pct_14.5m', 'do_ppm_14.5m')
